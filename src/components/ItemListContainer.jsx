82ed8b091a072a44659fc0ca41ac131bb03dccb3
SMelian <sofia.melian@usal.edu.ar>
2023-10-23 22:28:12 +0200

    Arreglo de key en item list

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,21 +1,19 @@
 function ItemListContainer(props) {
-   console.log (props.state)
+  console.log(props.state);
   return (
     <div className="item-list-container">
-       {props.state.map((item) => {
-         
+      {props.state.map((item) => {
         return (
-          <article className="card">
-          <h2 className="card__title">{item.flavor}</h2>
-          <img className="card__image" src={item.picture} alt={item.title} />
-          <p>${item.price}</p>
-          <button className="btnCard">ver mas</button>
-        </article>
-        )
+          <article key={item.id} className="card"> {/* Asigna una key única, como item.id */}
+            <h2 className="card__title">{item.flavor}</h2>
+            <img className="card__image" src={item.picture} alt={item.title} />
+            <p>${item.price}</p>
+            <button className="btnCard">ver mas</button>
+          </article>
+        );
       })}
     </div>
   );
 }
 
 export default ItemListContainer;
-
